047ff8e3da75f7c905557519b2fb73b6935208ae
kauane <kauanesantos.coding@gmail.com>
2023-08-01 15:09:04 -0300

    error and success message

--- src/Page/Main/index.js
+++ src/Page/Main/index.js
@@ -3,6 +3,10 @@ import { useState } from 'react'
 
 function Main() {
 
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+
   const [form, setForm] = useState([{
     email: '',
     password: ''
@@ -10,25 +14,50 @@ function Main() {
   }])
 
 
+  function handleChangeForms(e) {
+    const value = e.target.value
+    setForm({ ...form, [e.target.id]: value })
+  }
+
+  function handleSubmit(event) {
+
+  }
+
   return (
 
     <div className="container">
 
-      <form>
+      {success !== '' ? <span className='success'>{success}</span> : ''}
+      {error !== '' ? <span className='error'>{error}</span> : ''}
+
+
+      <form onSubmit={handleSubmit}>
         <h1>login</h1>
 
         <input
           type='text'
           placeholder='Type your e-mail'
           value={form.email}
+          onChange={(event) => handleChangeForms(event)}
         />
 
         <input
-          type='password'
+          type={showPassword ? 'text' : 'password'}
           placeholder='type your password'
           value={form.password}
-
+          onChange={(event) => handleChangeForms(event)}
         />
+
+        <div>
+          <input
+            type='checkbox'
+            name='show password'
+            checked={showPassword}
+            onChange={() => setShowPassword(!showPassword)}
+          />
+
+          {/* <label htmlFor='show-password'>Show password</label> */}
+        </div>
 
         <button type='submit'>Log in</button>
 
